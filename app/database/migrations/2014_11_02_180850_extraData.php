1afad05354ddf60ffe7c91c0a126188c8e057c96
johnrahme <john.rahme.se@gmail.com>
2014-11-03 01:49:29 +0100

    Added stuff

---
+++ app/database/migrations/2014_11_02_180850_extraData.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ExtraData extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('extraData', function($table){
+            $table->increments('id');
+            $table->integer('registrationsId')->unsigned();
+            $table->foreign('registrationsId')->references('id')->on('registrations');
+            $table->integer('extraFromControlId')->unsigned();
+            $table->foreign('extraFromControlId')->references('id')->on('extraFormControl');
+            $table->string('data');
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
