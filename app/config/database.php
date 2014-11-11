2993fcdbd52f07b8f58ef4a15d22334696cb4fc6
morrans <martinp1@live.se>
2014-11-11 19:14:29 +0100

    Testing

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'laravel',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'test',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
