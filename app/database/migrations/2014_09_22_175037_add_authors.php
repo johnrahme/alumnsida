cbf63a7d13edc59d9ac626c4eaa0154cd1a0dbbe
johnrahme <john.rahme.se@gmail.com>
2015-03-18 01:14:17 +0100

    ...

--- app/database/migrations/2014_09_22_175037_add_authors.php
+++ app/database/migrations/2014_09_22_175037_add_authors.php
@@ -12,36 +12,7 @@ class AddAuthors extends Migration {
 	 */
 	public function up()
 	{
-		DB::table('users')->insert(array(
-			'name' => 'John DS',
-			'email' => 'john.DSA.se@gmail.com',
-			'created_at' => date('Y-m-d H:m:s'),
-			'updated_at' => date('Y-m-d H:m:s')
-		));
-		DB::table('users')->insert(array(
-			'name' => 'John as',
-			'email' => 'john.rahme.se@DSF.com',
-			'created_at' => date('Y-m-d H:m:s'),
-			'updated_at' => date('Y-m-d H:m:s')
-		));
-		DB::table('users')->insert(array(
-			'name' => 'DS Rahme',
-			'email' => 'john.rahme.se@DS.com',
-			'created_at' => date('Y-m-d H:m:s'),
-			'updated_at' => date('Y-m-d H:m:s')
-		));
-		DB::table('users')->insert(array(
-			'name' => 'aA Rahme',
-			'email' => 'john.DSA.se@gmail.com',
-			'created_at' => date('Y-m-d H:m:s'),
-			'updated_at' => date('Y-m-d H:m:s')
-		));
-		DB::table('users')->insert(array(
-			'name' => 'DSa Rahme',
-			'email' => 'john.rahme.se@gmail.DS',
-			'created_at' => date('Y-m-d H:m:s'),
-			'updated_at' => date('Y-m-d H:m:s')
-		));
+
 	}
 
 	/**
@@ -51,11 +22,7 @@ class AddAuthors extends Migration {
 	 */
 	public function down()
 	{
-		DB::table('users') -> where('name', '=', "John DS")-> delete();
-		DB::table('users') -> where('name', '=', "John as")-> delete();
-		DB::table('users') -> where('name', '=', "DS Rahme")-> delete();
-		DB::table('users') -> where('name', '=', "aA Rahme")-> delete();
-		DB::table('users') -> where('name', '=', "DSa Rahme")-> delete();
+
 	}
 
 }
