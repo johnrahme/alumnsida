c36eb09571f2383a0126b75c838a141561cce6db
8408323 <j.haraldsson94@gmail.com>
2015-09-28 12:46:30 +0200

    Scroll sidled
    Fixat så det inte går att scrolla i sidled

--- futfhemsida/app/views/layouts/default.blade.php
+++ futfhemsida/app/views/layouts/default.blade.php
@@ -22,7 +22,7 @@
 <div id = "wrap">
     <div class = "navbar navbar-inverse navbar-static-top">
         <div class = "container">
-            <div class = "navbar-header">
+            <!--<div class = "navbar-header">
                 <a href = "{{url('/')}}" class = "navbar-brand">{{ HTML::image(URL::asset('img/TuppStorR.png'),'banner', array('class'=>'img-responsive', 'style'=>'height: 187%')) }}</a>
                 <a href = "{{url('/')}}" class = "navbar-brand">Föreingen Uppsala Tekniska Fysiker</a>
                 <a href = "{{url('/')}}" class = "navbar-brand">{{ HTML::image(URL::asset('img/TuppStor.png'),'banner', array('class'=>'img-responsive', 'style'=>'height: 187%')) }}</a>
@@ -32,7 +32,7 @@
                     <span class = "icon-bar"> </span>
 
                 </button>
-            </div>
+            </div>-->
             <div class = "navbar-collapse collapse navHeaderCollapse" id ="cssmenu">
                 <ul class = "nav navbar-nav navbar-right">
                     @if(Auth::check())
