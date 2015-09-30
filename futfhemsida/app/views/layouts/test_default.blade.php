3ed055a4a856f00fe5ff0fa1e0d47e3b045c7c5f
johnrahme <john.rahme.se@gmail.com>
2015-09-30 21:09:40 +0200

    DropdownTest working

--- futfhemsida/app/views/layouts/test_default.blade.php
+++ futfhemsida/app/views/layouts/test_default.blade.php
@@ -31,8 +31,8 @@
             <div class = "navbar-collapse collapse navHeaderCollapse">
                 <ul class = "nav navbar-nav navbar-left">
                     <li class = "dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Dropdown <span class="caret"></span></a>
-                                    <ul class="dropdown-menu pull-center">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
                                       <li><a href="#">Action</a></li>
                                       <li><a href="#">Another action</a></li>
                                       <li><a href="#">Something else here</a></li>
