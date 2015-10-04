b0afceabbeed447670784f55a9b5bcd2d4c758d6
johnrahme <john.rahme.se@gmail.com>
2015-10-04 03:20:31 +0200

    Inga mellanslag i länk

--- futfhemsida/public/js/menu/menu.js
+++ futfhemsida/public/js/menu/menu.js
@@ -9,9 +9,9 @@ $('#parent').change(function(){
 function update(){
     if($('#parent').val()!=''){
         var id = $('#parent').children(":selected").attr("id");
-        $('#url').val((id+'/'+$('#name').val()).toLocaleLowerCase());
+        $('#url').val((id+'/'+$('#name').val()).toLocaleLowerCase().replace(/\s+/g, ''));
     }
     else {
-        $('#url').val($('#name').val().toLowerCase());
+        $('#url').val($('#name').val().toLowerCase().replace(/\s+/g, ''));
     }
 }
