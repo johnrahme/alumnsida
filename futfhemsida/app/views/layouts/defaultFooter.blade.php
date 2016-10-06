3e517a73f2ac54c4b8e87162fae475b565df5d9d
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2016-10-06 14:04:58 +0200

    Fixat tid och datum i footern
    Fixat tid och datum i footern, så att det alltid är på svenska.

--- futfhemsida/app/views/layouts/defaultFooter.blade.php
+++ futfhemsida/app/views/layouts/defaultFooter.blade.php
@@ -1,3 +1,5 @@
+{{ HTML::script('js/footerDate.js')}}
+
 <div class="footer">
     <div class="container">
         <div id="largeElement" class="row">
@@ -12,36 +14,13 @@
                 </div>
             </div>
             <div class="col-sm-4">
-                <div style="text-align: center; width:100%; margin-top: 0px ">
-                    <SCRIPT LANGUAGE="JavaScript"
-                            TYPE="text/javascript">
-                        var date = new Date();
-                        var option1 = {weekday: "long"};
-                        var option2 = {day: "numeric"};
-                        var option3 = {month: "long"};
-                        document.write(date.toLocaleString("se-SE", option1), "en den ", date.toLocaleString("se-SE", option2), " ", date.toLocaleString("se-SE", option3), "<br>");
-                        function startTime() {
-                            var today = new Date();
-                            var h = today.getHours();
-                            var m = today.getMinutes();
-                            var s = today.getSeconds();
-                            m = checkTime(m);
-                            s = checkTime(s);
-                            document.getElementById('time').innerHTML =
-                                    h + ":" + m + ":" + s;
-                            var t = setTimeout(startTime, 999);
-                        }
-                        function checkTime(i) {
-                            if (i < 10) {
-                                i = "0" + i
-                            }
-                            ;  // add zero in front of numbers < 10
-                            return i;
-                        }
-                    </SCRIPT>
-                    <body onload="startTime()">
-                    <div id="time"></div>
+                <div style="text-align: center; width:100%; margin-top: 5px ">
+
+                    <body>
+                    <span id="date_time"></span>
+                    <script type="text/javascript">window.onload = date_time('date_time');</script>
                     </body>
+
                     <h6 style="margin-top: -8px"><br> Skapad från <span class="glyphicon glyphicon-heart"></span>
                         av {{ link_to('/','the J-Quad Squad', array('class' => 'colourLink'))}}.</h6>
                     <p><h6>Copyright &copy; {{date("Y")}} FUTF.</h6></p>
@@ -90,38 +69,14 @@
                         av {{ link_to('/','the J-Quad Squad', array('class' => 'colourLink'))}}.</h6>
 
                     <p><h6>Copyright &copy; {{date("Y")}} FUTF.</h6></p>
-                    <SCRIPT LANGUAGE="JavaScript"
-                            TYPE="text/javascript">
-                        var date = new Date();
-                        var option1 = {weekday: "long"};
-                        var option2 = {day: "numeric"};
-                        var option3 = {month: "long"};
-                        document.write(date.toLocaleString("se-SE", option1), "en den ", date.toLocaleString("se-SE", option2), " ", date.toLocaleString("se-SE", option3), "<br>");
-                        function startTime() {
-                            var today = new Date();
-                            var h = today.getHours();
-                            var m = today.getMinutes();
-                            var s = today.getSeconds();
-                            m = checkTime(m);
-                            s = checkTime(s);
-                            document.getElementById('time').innerHTML =
-                                    h + ":" + m + ":" + s;
-                            var t = setTimeout(startTime, 500);
-                        }
-                        function checkTime(i) {
-                            if (i < 10) {
-                                i = "0" + i
-                            }
-                            ;  // add zero in front of numbers < 10
-                            return i;
-                        }
-                    </SCRIPT>
-                    <body onload="startTime()">
-                    <div style="padding: 2px" id="time"></div>
+
+                    <body>
+                    <span id="date_time"></span>
+                    <script type="text/javascript">window.onload = date_time('date_time');</script>
                     </body>
+
                 </div>
             </div>
         </div>
-    </div>
     </div>
 </div>
