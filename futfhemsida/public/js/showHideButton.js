310c11e26c916c1a3265ddb0a57abfc71e5614bd
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2016-10-07 12:13:29 +0200

    Tagit bort gamla showHideButton

--- futfhemsida/public/js/showHideButton.js
+++ futfhemsida/public/js/showHideButton.js
@@ -1,15 +1,3 @@
-/*$(document).ready(function () {
-    $("#hideandshow").click(function () {
-        $("#hideAndShow").slideToggle('slow');
-    });
-});
-function changeImage(element) {
-    var down = "http://i67.tinypic.com/15ias5i.jpg";
-    var up = "http://i64.tinypic.com/jqhxer.jpg";
-    element.src = element.bln ? down : up;
-    element.bln = !element.bln;
-}*/
-
 $(document).ready(function() {
     $('#toggle1').click(function() {
         $('#hideAndShow').slideToggle('slow');
