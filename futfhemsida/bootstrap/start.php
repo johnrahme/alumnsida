2c849f0dc08f257fc00bf3eb9b391f5aa9873d90
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2017-02-07 18:25:38 +0100

    Added Johan's PC
    Added Johan's PC

--- futfhemsida/bootstrap/start.php
+++ futfhemsida/bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-    'local' => array('JohnStationary', 'Jakobiloben', 'Jonathans', 'Soodlas', 'Bastion', 'User-PC'),
+    'local' => array('JohnStationary', 'Jakobiloben', 'Jonathans', 'Soodlas', 'Bastion', 'User-PC', 'LAPTOP-1EK3NR3A'),
 
 ));
 
