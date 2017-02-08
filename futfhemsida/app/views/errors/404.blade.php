c3b32aa7bbcca210b082981c2057d8dc42302cea
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2017-02-08 03:28:30 +0100

    Missade en fil till 404-errorsidan

---
+++ futfhemsida/app/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.default')
+
+@section('styles')
+    {{--{{ HTML::style('css/owncloud.css') }}--}}
+@stop
+
+@section('content')
+
+    <img src="img/404_error_page.png" height="100%" width="100%"/>
+
+@stop
+
+@section('scripts')
+    {{--    {{ HTML::script('js/snake/snake.js') }}--}}
+@stop
