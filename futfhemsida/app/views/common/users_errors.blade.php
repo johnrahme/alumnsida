cc7c52bfd57682f1a0feb4cfba0f0fa0ec905aa8
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2017-02-08 01:38:29 +0100

    Lagt till margin-left för snyggare layout på error message
    Lagt till margin-left för snyggare layout på error message

--- futfhemsida/app/views/common/users_errors.blade.php
+++ futfhemsida/app/views/common/users_errors.blade.php
@@ -1,11 +1,11 @@
 @if($errors->has())
-    <ul>
+    <ul style="margin-left: 17px;">
         {{$errors->first('password', '<li style = "color: red;">:message </li>')}}
+        {{$errors->first('name', '<li style = "color: red;">:message</li>')}}
+        {{$errors->first('surname', '<li style = "color: red;">:message</li>')}}
         {{$errors->first('email', '<li style = "color: red;">:message</li>')}}
         {{$errors->first('username', '<li style = "color: red;">:message</li>')}}
         {{$errors->first('agreement', '<li style = "color: red;">:message</li>')}}
-        {{$errors->first('surname', '<li style = "color: red;">:message</li>')}}
-        {{$errors->first('name', '<li style = "color: red;">:message</li>')}}
         {{$errors->first('data', '<li style = "color: red;">:message</li>')}}
     </ul>
 
