9abb2e9dd6734810f0459f0fa063d7f3e983b883
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2017-03-24 10:43:02 +0100

    Changed the design of sidemenupanels and fixed so the menu does not disappear onclick
    Changed the design of sidemenupanels and fixed so the menu does not
    disappear onclick

--- futfhemsida/public/js/sidemenupanel.js
+++ futfhemsida/public/js/sidemenupanel.js
@@ -2,7 +2,6 @@ var ww = document.body.clientWidth;
 
 $(document).ready(function () {
     $(".toggleMenu").click(function (e) {
-        e.preventDefault();
         $(this).toggleClass("active");
         $(".sidemenupanel").toggle();
     });
@@ -15,13 +14,12 @@ $(window).bind('resize orientationchange', function () {
 });
 
 var adjustMenu = function () {
-    if (ww >= 0) {
+    if (ww >= 0 ) {
         $(".toggleMenu").css("display", "none");
         $(".sidemenupanel").show();
         $(".sidemenupanel li").removeClass("hover");
         $(".sidemenupanel li a").unbind('click');
         $(".sidemenupanel li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function () {
-            // must be attached to li so that mouseleave is not triggered when hover over submenu
             $(this).toggleClass('hover');
         });
     }
