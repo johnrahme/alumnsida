8e5d3ccf9220b581766fac5489e9df4c39387df3
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2017-04-25 00:26:38 +0200

    Fixed borken OwnCloud links
    Fixed borken OwnCloud links

--- futfhemsida/app/views/ownCloud/index.blade.php
+++ futfhemsida/app/views/ownCloud/index.blade.php
@@ -3,19 +3,21 @@
 @section('styles')
     {{ HTML::style('css/owncloud.css') }}
 @stop
+<style>
+    @media (min-width: 768px) {
+        #adjust {
+            left: -187.5px;{{-- -187.5 px to replace the column of width 3 to the left (col-sm-3 =3*750px/12=187.5 px) on big screen--}}
+        }
+    }
+</style>
 
 @section('content')
-    @if(Auth::check()) <a href="beta.futf.se/owncloud/index.php">Till inloggningen av OwnCloud.</a>
+    @if(Auth::check()) <a id="adjust" style="position: relative" href="/owncloud">Till inloggningen av OwnCloud.</a>
     @else
     @endif
 
-    <div class="futf-documents-wrapper panel panel-default">
+    <div id="adjust" class="futf-documents-wrapper panel panel-default" style="width: 100%; position:relative;">
         <iframe class="futf-documents"
-                src="http://beta.futf.se/owncloud/public.php?service=files&t=bb6e7dc555946d24a5546686f447fe72"></iframe>
+                src="http://beta.futf.se/owncloud/public.php?service=files&t=d887b8a22ef2f6ec6380bb5e6a43c467"></iframe>
     </div>
-
-@stop
-
-@section('scripts')
-    {{ HTML::script('js/snake/snake.js') }}
 @stop
