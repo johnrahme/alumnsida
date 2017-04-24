c351070eac80f1d1cdcebf72681499d2cc29ed27
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2017-04-24 23:11:35 +0200

    Added carousel on samarbetspartners
    Added carousel on samarbetspartners

--- futfhemsida/app/views/samarbetspartners/samarbetspartners_start.blade.php
+++ futfhemsida/app/views/samarbetspartners/samarbetspartners_start.blade.php
@@ -7,22 +7,45 @@
  */
 
 ?>
-<!DOCTYPE html>
+        <!DOCTYPE html>
 <html>
 <head>
-<title> {{$title}} </title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title> {{$title}} </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <style>
+        @media (max-width: 768px) {
+            .samarbetspartners_slideshow {
+                display: none;
+            }
 
+            .animate-right {
+                position: relative;
+                animation: animateright 0.4s
+            }
+
+            @keyframes animateright {
+                from {
+                    right: -300px;
+                    opacity: 0
+                }
+                to {
+                    right: 0;
+                    opacity: 1
+                }
+            }
+        }
+    </style>
+</head>
 <div id="id_företag" style="padding-top: 20px; display:none;">
     <div class="panel panel-default">
         <h4 style="text-align: center; ">Samarbetspartners</h4>
         <div>
             <table style="width: 100%">
                 <?php $samarbetspartners = Samarbetspartners::orderBy('created_at', 'desc')->get(); ?>
-                @foreach($samarbetspartners as$key => $currSp)
-                    <tr>
-                        <th @if($currSp->url != 'empty')style="width: 200px; height: 200px"@endif>@if($currSp->url != 'empty'){{HTML::image($currSp->url, '', array('class' => 'img-responsive'))}}@endif</th>
+                @foreach($samarbetspartners as $key => $currSp)
+                    <tr class="samarbetspartners_slideshow animate-right">
+                        <th @if($currSp->url != 'empty')style="height: 200px"@endif>@if($currSp->url != 'empty'){{HTML::image($currSp->url, '', array('class' => 'img-responsive'))}}@endif</th>
                     </tr>
                 @endforeach
             </table>
@@ -31,3 +54,25 @@
         </div>
     </div>
 </div>
+
+<script>
+    var index = 0;
+    carousel();
+
+    function carousel() {
+        if (window.innerWidth <= 768) {
+            var i;
+            var x = document.getElementsByClassName("samarbetspartners_slideshow");
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            index++;
+            if (index > x.length) {
+                index = 1
+            }
+            x[index - 1].style.display = "block";
+            setTimeout(carousel, 5000); // Change image every 5 seconds
+        }
+    }
+</script>
+</html>
