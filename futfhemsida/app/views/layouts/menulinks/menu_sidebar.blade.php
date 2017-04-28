d421a2499cceabf8c60c77c8eafd4da334f5ccd2
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2017-04-28 17:28:11 +0200

    Hittade en bugg, revertade tillbaka till en fungerande version. Ska kolla mer på den vid tillfälle. Nu fungerar inte menyerna i sidomenyn at
    Hittade en bugg, revertade tillbaka till en fungerande version. Ska
    kolla mer på den vid tillfälle. Nu fungerar inte menyerna i sidomenyn
    att byta plats på..

--- futfhemsida/app/views/layouts/menulinks/menu_sidebar.blade.php
+++ futfhemsida/app/views/layouts/menulinks/menu_sidebar.blade.php
@@ -1,34 +1,47 @@
-@if(!is_null($activeMenu))
-    <ul>
-        <?php
-        $menus = Menu::orderBy('order')->get();
-        ?>
-        @foreach($menus as $menu)
-            @if($menu->online == 1)
+@if(Auth::check())
+    @if(!is_null($activeMenu))
+        <ul>
+            @foreach($currentSubMenus as $currentSubMenu)
                 <?php
-                $currentSubMenus = Submenu::where('menuId', '=', $menu->id)->orderBy('order')->get();
+                $currentSubSubMenus = Subsubmenu::where('subMenuId', '=', $currentSubMenu->id)->orderBy('order')->get();
                 ?>
-                @foreach($currentSubMenus as $currentSubMenu)
-                    <?php
-                    $currentSubSubMenus = Subsubmenu::where('subMenuId', '=', $currentSubMenu->id)->orderBy('order')->get();
-                    ?>
-                    @if($currentSubMenu->online == 1)
-                        <li id="{{$currentSubMenu->url}}2"> {{link_to($currentSubMenu->url,$currentSubMenu->name)}}
-                            <ul class="blockSidemenuPanel_ul">
-                                @foreach($currentSubSubMenus as $currentSubSubMenu)
-                                    @if($currentSubSubMenu->online == 1)
-                                        <li class="blockSidemenuPanel_li">
-                                            {{link_to($currentSubSubMenu->url,$currentSubSubMenu->name)}}
-                                            <ul class="test"></ul>
-                                        </li>
-                                    @endif
-                                @endforeach
-                            </ul>
-                        </li>
-                    @endif
+                <li id="{{$currentSubMenu->url}}2"> {{link_to($currentSubMenu->url,$currentSubMenu->name)}}
+                    <ul class="blockSidemenuPanel_ul">
+                        @foreach($currentSubSubMenus as $currentSubSubMenu)
+                            <li class="blockSidemenuPanel_li">
+                                {{link_to($currentSubSubMenu->url,$currentSubSubMenu->name)}}
+                                <ul class="test"></ul>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+@endif
 
-                @endforeach
-            @endif
-        @endforeach
-    </ul>
+@if(!Auth::check())
+    @if(!is_null($activeMenu))
+        <ul>
+            @foreach($currentSubMenus as $currentSubMenu)
+                <?php
+                $currentSubSubMenus = Subsubmenu::where('subMenuId', '=', $currentSubMenu->id)->orderBy('order')->get();
+                ?>
+                @if($currentSubMenu->online == 1)
+                    <li id="{{$currentSubMenu->url}}2"> {{link_to($currentSubMenu->url,$currentSubMenu->name)}}
+                        <ul class="blockSidemenuPanel_ul">
+                            @foreach($currentSubSubMenus as $currentSubSubMenu)
+                                @if($currentSubSubMenu->online == 1)
+                                    <li class="blockSidemenuPanel_li">
+                                        {{link_to($currentSubSubMenu->url,$currentSubSubMenu->name)}}
+                                        <ul class="test"></ul>
+                                    </li>
+                                @endif
+                            @endforeach
+                        </ul>
+                    </li>
+                @endif
+            @endforeach
+        </ul>
+    @endif
 @endif
