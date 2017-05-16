8715dcaf13854c6d57a91b4f3ff28d0db4ea2df6
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2017-05-16 20:35:46 +0200

    Fixat till styrelsesidan, så att den inte täcker över menyn
    Fixat till styrelsesidan, så att den inte täcker över menyn

--- futfhemsida/app/views/admin/viewPublic.blade.php
+++ futfhemsida/app/views/admin/viewPublic.blade.php
@@ -1,14 +1,6 @@
 @extends('layouts.default')
 @section('content')
-    <style>
-        @media (min-width: 768px) {
-            #adjust {
-                left: -187.5px;
-            {{-- -187.5 px to replace the column of width 3 to the left (col-sm-3 =3*750px/12=187.5 px) on big screen--}}
-            }
-        }
-    </style>
-    <div id="adjust" style="position: relative;">
+    <div style="position: relative;">
         <div>
             <h1>
                 Styrelsen
