1508410e5fb65a2e2cef5e7ad0a7adb074b65a73
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2017-05-22 09:18:13 +0200

    Lagt till mig och Albin i creators. Tagit bort onödig fil (shaders.php), samt flyttat bilderna lokalt för att se om det fungerar på hemsidan
    Lagt till mig och Albin i creators. Tagit bort onödig fil (shaders.php),
    samt flyttat bilderna lokalt för att se om det fungerar på hemsidan

--- futfhemsida/app/views/creators/index.blade.php
+++ futfhemsida/app/views/creators/index.blade.php
@@ -5,6 +5,12 @@
         #adjust {
             left: -187.5px;
         {{-- -187.5 px to replace the column of width 3 to the left (col-sm-3 =3*750px/12=187.5 px) on big screen--}}
+
+
+
+
+
+
         }
     }
 </style>
@@ -26,11 +32,17 @@
             <div class="panel panel-default">
                 <div class="panel-body" style="padding-top: 0">
                     <img style="margin-top: 5px;" class="img-responsive"
-                         src="http://i.imgur.com/Y9lhfmX.jpg"/> {{-- Fixa så filen kan ligga lokalt, lcykas inte få access till bilden just nu..--}}
+                         src="http://beta.futf.se/img/creators/Jonathan.jpg"/> {{-- Fixa så filen kan ligga lokalt, lcykas inte få access till bilden just nu..--}}
                     <div class="page-header" style="margin-top:0px">
                         <h4 style="text-align: center">Jonathan Haraldsson</h4>
                     </div>
-                    Beskrivning här!
+                    Jag har varit med i IT-gruppen sedan start, hösten 2015. Jag har lärt mig mycket av att vara med
+                    och utveckla hemsidan från start. Jag var informationsansvarig mellan juli 2016 och juni 2017,
+                    vilket bidrog till en ledarroll inom IT-gruppen. Jag har utvecklat mycket av designen, menyerna,
+                    felhantering, samarbetspartners, fillagringssystemet
+                    (<a href="http://beta.futf.se/files" target="_blank">Dokument</a>),
+                    sidfoten, en del småsaker och en massa annat. Jag är väldigt tacksam för att jag fått varit med i
+                    arbetet med denna hemsida. Hoppas Ni tycker om hemsidan lika mycket som jag gör.
                 </div>
             </div>
         </div>
@@ -48,11 +60,17 @@
         <div class="col-sm-3">
             <div class="panel panel-default">
                 <div class="panel-body" style="padding-top: 0">
-                    {{--<img style="margin-top: 5px;" class="img-responsive" src="img_link_here"/> --}}{{-- Fixa så filen kan ligga lokalt, lcykas inte få access till bilden just nu..--}}
+                    <img style="margin-top: 5px;" class="img-responsive"
+                         src="http://beta.futf.se/img/creators/Albin.jpg"/> {{-- Fixa så filen kan ligga lokalt, lcykas inte få access till bilden just nu..--}}
                     <div class="page-header" style="margin-top:0px">
                         <h4 style="text-align: center">Albin Gideonsson</h4>
                     </div>
-                    Beskrivning här!
+                    Som den nyaste medlemmen i IT-gruppen har jag egentligen gjort ganska lite faktiskt arbete, utan
+                    istället stöttat de andra med idéer och tappra försök till problemlösning. Jag var ansvarig för
+                    nyheterna som syns på startsidan samt nyhetsarkivets generella layout, och jag kan stolt säga att
+                    jag är nöjd med mitt jobb. Till sist vill jag tacka Jonathan, och alla andra, för att jag fick gå
+                    med i IT-gruppen. Det har varit ett oerhört lärorikt år, ett projekt som länge väntat på att bli
+                    klart och jag är väldigt glad att ha hunnit vara en del av det. Tack.
                 </div>
             </div>
         </div>
