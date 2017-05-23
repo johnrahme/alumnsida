49881ef9a356b2e0a2f4fa6f28ebd5e6daa17dcd
Jonathan Haraldsson <j.haraldsson94@gmail.com>
2017-05-23 21:03:50 +0200

    Uppdaterat footern
    Uppdaterat footern

--- futfhemsida/app/views/layouts/defaultFooter.blade.php
+++ futfhemsida/app/views/layouts/defaultFooter.blade.php
@@ -28,7 +28,8 @@
                         </body>
 
                         <h6 style="margin-top: -8px"><br> Skapad från <span class="glyphicon glyphicon-heart"></span>
-                            av {{ link_to('creators','FUTF:s IT-grupp 2015-2017', array('class' => 'colourLink'))}}.</h6>
+                            av {{ link_to('creators','FUTF:s IT-team 2015-2017', array('class' => 'colourLink'))}}.
+                        </h6>
                         <p><h6>Copyright &copy; {{date("Y")}} FUTF.</h6></p>
                     </div>
                 </div>
@@ -49,11 +50,15 @@
             <div id="smallElement" class="row">
                 <div class="col-sm-4">
                     <div style="text-align: center; width:100%;">
-                        <h6>Navigering</h6>
-                        <ul class="unstyled">
-                            <li>{{link_to('/','Start')}}</li>
-                            <li>{{link_to_route('events', 'Event')}}</li>
-                            <li>{{link_to_route('new_admin', 'Skapa konto')}}</li>
+                        <h4>Postadress</h4>
+                        <ul class="unstyled" style="margin-top: -10px">
+                            <li>FUTF, Uthgård</li>
+                            <li>752 37 Uppsala</li>
+                        </ul>
+                        <h4>Besöksadress</h4>
+                        <ul class="unstyled" style="margin-top: -10px">
+                            <li>Polacksbacken ITC</li>
+                            <li>Hus 1, rum 1153</li>
                         </ul>
                     </div>
                 </div>
@@ -72,7 +77,8 @@
                 <div class="col-sm-4">
                     <div style="text-align: center; width:100%">
                         <h6><br> Skapad från <span class="glyphicon glyphicon-heart"></span>
-                            av {{ link_to('creators','FUTF:s IT-grupp 2015-2017', array('class' => 'colourLink'))}}.</h6>
+                            av {{ link_to('creators','FUTF:s IT-team 2015-2017', array('class' => 'colourLink'))}}.
+                        </h6>
                         <p><h6>Copyright &copy; {{date("Y")}} FUTF.</h6></p>
                     </div>
                 </div>
