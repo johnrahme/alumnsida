6e510ce05fc3397056138f801e933e95b0b2659a
Simon Blomberg <simpan22@gmail.com>
2017-09-15 16:40:41 +0200

    Ändrat lite på startsidan

--- futfhemsida/app/views/start/index.blade.php
+++ futfhemsida/app/views/start/index.blade.php
@@ -9,7 +9,7 @@
 @section('content')
     {{--Jumbotron--}}
     <div class="jumbotron text-center">
-        <h2> Välkommen till betaversionen av FUTF:s nya hemsida!</h2>
+        <h2> Välkommen till FUTF:s nya hemsida!</h2>
 
         <p> Hör gärna av er med idéer till {{HTML::mailto('it@futf.se')}}, vi vill ha all feedback vi kan få. Om du inte känner dig redo
         för den nya hemsidan klicka <a href="http://old.futf.se">här</a> för att komma till den gamla hemsidan.</p>
